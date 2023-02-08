c12449098faa35454e4ad0ca5809ac5f7f40aa96
N1ght0m <tomasgeorge79@gmail.com>
2023-02-08 20:56:18 -0300

    added bring profile

--- src/main/java/com/Portfolio/Web/gat/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/Web/gat/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.Portfolio.Web.gat.Entity.Persona;
 import com.Portfolio.Web.gat.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,15 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("/persona/get")
+    @GetMapping("/personas/get")
     public List<Persona> getPersona() {
         return ipersonaService.getPersona();
     }
     
-    @PostMapping("/personas/ceate")
+    @PostMapping("/personas/create")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
@@ -47,5 +49,10 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/bring/profile")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
