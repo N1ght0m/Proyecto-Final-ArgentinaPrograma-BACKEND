a9fbfbc610f9f6f1cc6c3dae9d446217c2d2ad75
N1ght0m <tomasgeorge79@gmail.com>
2023-02-12 00:28:26 -0300

    finished JWT

---
+++ src/main/java/com/Portfolio/Web/gat/Security/Service/RolService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Portfolio.Web.gat.Security.Service;
+
+import com.Portfolio.Web.gat.Security.Entity.Rol;
+import com.Portfolio.Web.gat.Security.Enums.RolNombre;
+import com.Portfolio.Web.gat.Security.Repository.iRolRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
