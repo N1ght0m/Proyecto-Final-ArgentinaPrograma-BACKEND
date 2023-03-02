8a02b08eca75aa45d884e3262727f6c62eb1980e
N1ght0m <tomasgeorge79@gmail.com>
2023-03-02 18:17:58 -0300

    added Edit and delete for education

--- src/main/java/com/portfolio/gat/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/gat/Controller/CExperiencia.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("explab")
+@RequestMapping("/explab")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CExperiencia {
 
@@ -37,6 +38,7 @@ public class CExperiencia {
         if (!sExperiencia.existsById(id)) {
             return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
         }
+
         Experiencia experiencia = sExperiencia.getOne(id).get();
         return new ResponseEntity(experiencia, HttpStatus.OK);
     }
@@ -82,16 +84,13 @@ public class CExperiencia {
 
     }
 
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
-
         if (!sExperiencia.existsById(id)) {
-            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
         }
-
         sExperiencia.delete(id);
-
-        return new ResponseEntity(new Mensaje("Experiencia eliminada"), HttpStatus.OK);
-
+        return new ResponseEntity(new Mensaje("producto eliminado"), HttpStatus.OK);
     }
 
 }
