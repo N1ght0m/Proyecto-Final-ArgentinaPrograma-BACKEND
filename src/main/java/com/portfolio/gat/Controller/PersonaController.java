0aec16dde1a9448a17900e1218f9bd5dcb99e6a9
N1ght0m <tomasgeorge79@gmail.com>
2023-03-11 15:11:23 -0300

    msg02

--- src/main/java/com/portfolio/gat/Controller/PersonaController.java
+++ src/main/java/com/portfolio/gat/Controller/PersonaController.java
@@ -13,6 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,23 @@ public class PersonaController {
     public ResponseEntity<List<Persona>> list() {
         List<Persona> list = personaService.list();
         return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona){
+        if(StringUtils.isBlank(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(personaService.existsByNombre(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("Ese nombre existe"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = new Persona(
+                dtopersona.getNombre(), dtopersona.getApellido(), dtopersona.getDescripcion(), dtopersona.getImg()
+            );
+        personaService.save(persona);
+        return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
+                
     }
 
     @GetMapping("/detail/{id}")
